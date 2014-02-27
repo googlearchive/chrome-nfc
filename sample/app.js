a71e575710a279f4a6f1c0082d1ec1eb2ccc4c0e
François Beaufort <beaufort.francois@gmail.com>
2014-02-27 14:51:27 +0100

    [sample] Handle timeout errors

--- sample/app.js
+++ sample/app.js
@@ -26,51 +26,75 @@ function log(message, object) {
   document.querySelector('#logContainer').classList.remove('small');
 }
 
-function readNdefTag() {
+function handleDeviceTimeout(func, args) {
+  var timeoutInMs = 1000;
+  var hasTags = false;
+  setTimeout(function() {
+    if (!hasTags) {
+      log('Timeout! No tag detected');
+    }
+  }, timeoutInMs);
+  var args = args || [];
+  args = args.concat([function() { hasTags = true; }]);
+  func.apply(this, args);
+}
+
+function onReadNdefTagButtonClicked() {
+  handleDeviceTimeout(readNdefTag);
+}
+
+function readNdefTag(callback) {
   chrome.nfc.read(device, {}, function(type, ndef) {
-    log('Found ' + ndef.ndef.length + ' NDEF Tag(s)');
+    log('Found ' + ndef.ndef.length + ' NFC Tag(s)');
     for (var i = 0; i < ndef.ndef.length; i++)
-      log('NDEF Tag', ndef.ndef[i]);
+      log('NFC Tag', ndef.ndef[i]);
+    callback();
   });
 }
 
-function readMifareTag() {
+function onReadMifareTagButtonClicked() {
+  handleDeviceTimeout(readMifareTag);
+}
+
+function readMifareTag(callback) {
   var blockNumber = 0; // starting logic block number.
   var blocksCount = 2; // logic block counts.
   chrome.nfc.read_logic(device, blockNumber, blocksCount, function(rc, data) {
     log('Mifare Classic Tag', UTIL_BytesToHex(data));
+    callback();
   });
 }
 
 function onWriteNdefTagButtonClicked() {
   var ndefType = document.querySelector('#write-ndef-type').value;
   var ndefValue = document.querySelector('#write-ndef-value').value;
-  writeNdefTag(ndefType, ndefValue);
+  handleDeviceTimeout(writeNdefTag, [ndefType, ndefValue]);
 }
 
-function writeNdefTag(ndefType, ndefValue) {
+function writeNdefTag(ndefType, ndefValue, callback) {
   var ndef = {};
   ndef[ndefType] = ndefValue;
   chrome.nfc.write(device, {"ndef": [ndef]}, function(rc) {
     if (!rc) {
-      log('NDEF Tag written!');
+      log('NFC Tag written!');
     } else {
-      log('NDEF Tag write operation failed', rc);
+      log('NFC Tag write operation failed', rc);
     }
+    callback();
   });
 }
 
 function onWriteMifareTagButtonClicked() {
   try {
     var mifareData = JSON.parse(document.querySelector('#mifare-data').value);
-    writeMifareTag(mifareData);
+    handleDeviceTimeout(writeMifareTag, [mifareData]);
   }
   catch(e) {
     log('Error', 'Mifare Data is not an Array.');
   }
 }
 
-function writeMifareTag(mifareData) {
+function writeMifareTag(mifareData, callback) {
   var data = new Uint8Array(mifareData);
   var blockNumber = 0; // starting logic block number.
   chrome.nfc.write_logic(device, 0, data, function(rc) {
@@ -79,24 +103,26 @@ function writeMifareTag(mifareData) {
     } else {
       log('Mifare Tag write operation failed', rc);
     }
+    callback();
   });
 }
 
 function onEmulateTagButtonClicked() {
   var ndefType = document.querySelector('#emulate-ndef-type').value;
   var ndefValue = document.querySelector('#emulate-ndef-value').value;
-  emulateTag(ndefType, ndefValue);
+  handleDeviceTimeout(emulateTag, [ndefType, ndefValue]);
 }
 
-function emulateTag(ndefType, ndefValue) {
+function emulateTag(ndefType, ndefValue, callback) {
   var ndef = {};
   ndef[ndefType] = ndefValue;
   chrome.nfc.emulate_tag(device, {"ndef": [ndef]}, function(rc) {
     if (!rc) {
-      log('NDEF Tag emulated!');
+      log('NFC Tag emulated!');
     } else {
-      log('NDEF Tag emulate operation failed', rc);
+      log('NFC Tag emulate operation failed', rc);
     }
+    callback();
   });
 }
 
@@ -136,10 +162,10 @@ function enumerateDevices() {
 enumerateDevices();
 
 document.querySelector('#read-ndef pre').textContent = readNdefTag.toString();
-document.querySelector('#read-ndef button').addEventListener('click', readNdefTag);
+document.querySelector('#read-ndef button').addEventListener('click', onReadNdefTagButtonClicked);
 
 document.querySelector('#read-mifare pre').textContent = readMifareTag.toString();
-document.querySelector('#read-mifare button').addEventListener('click', readMifareTag);
+document.querySelector('#read-mifare button').addEventListener('click', onReadMifareTagButtonClicked);
 
 document.querySelector('#write-ndef pre').textContent = writeNdefTag.toString();
 document.querySelector('#write-ndef button').addEventListener('click', onWriteNdefTagButtonClicked);
